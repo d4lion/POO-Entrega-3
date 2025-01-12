bfe319a1f08ead3e06e2719080fa26e702350e3c
d4lion <d4lion.dev@gmail.com>
2025-01-11 22:00:40 -0500

    🔒️ Refactor(Ejercicio7): Remove unnecessary initialization of text fields and update method call to private algorithm

--- src/pages/parte_1/capitulo_4/ejercicio_7/Solucion.py
+++ src/pages/parte_1/capitulo_4/ejercicio_7/Solucion.py
@@ -9,7 +9,6 @@ class Ejercicio7:
         self.text_fielda_B = TextField(
             label="Ingrese el valor de B",
         )
-        self.__init_text_fields()
 
     def __algoritmo_propuesto(self, valor_A: float, valor_B: float):
         if valor_A > valor_B:
@@ -37,7 +36,7 @@ class Ejercicio7:
                                 controls=[
                                     TextField(
                                         label="Resultado",
-                                        value=self.algoritmo_propuesto(
+                                        value=self.__algoritmo_propuesto(
                                             float(self.text_fielda_A.value),
                                             float(self.text_fielda_B.value)
                                         ),
